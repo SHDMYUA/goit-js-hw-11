489a64127c8b402c03d69eff4b7a143b76e3fcc0
SHDMYUA <90021141+SHDMYUA@users.noreply.github.com>
2022-06-29 09:59:08 +0000

    Deploying to gh-pages from @ SHDMYUA/goit-js-hw-11@41db6a7e33ce62dc376cfe2c91bcf27272c7346f 🚀

--- index.06a603c2.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault();var t=e.currentTarget.searchQuery.value,n="https://pixabay.com/api/?key=".concat("28024348-9ac2310d97b674b7de52a4e68","&q=").concat(t,"&image_type=photo");fetch(n).then((function(e){return e.json()})).then(console.log)}));
-//# sourceMappingURL=index.06a603c2.js.map
--- index.aadd6d15.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault();const{searchQuery:t}=e.currentTarget,a=t.value;fetch(`https://pixabay.com/api/?key=28024348-9ac2310d97b674b7de52a4e68&q=${a}&image_type=photo`).then((e=>e.json())).then(console.log)}));
-//# sourceMappingURL=index.aadd6d15.js.map
